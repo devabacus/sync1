c86d9d987b4fffa3a8ee8a95d95c67e3cbda0737
devabacus <32705417+devabacus@users.noreply.github.com>
2025-06-09 10:17:30 +0500

    delete some files

--- sync1_flutter/lib/main.dart
+++ sync1_flutter/lib/main.dart
@@ -11,7 +11,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized(); 
   await dotenv.load(fileName: ".env");
-  HttpOverrides.global = MyHttpOverrides();
+  // HttpOverrides.global = MyHttpOverrides();
 
   runApp(
   ProviderScope(
@@ -22,3 +22,4 @@ void main() async{
   )
  );
 }
+
